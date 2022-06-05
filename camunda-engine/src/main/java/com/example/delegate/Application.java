82305150be7c38c7c099f46f1e65c405c36606ba
Damianos Sotirakis <dansotirakis18@gmail.com>
2022-06-05 20:50:25 -0300

    CAMJ5-book-journey :  using camunda engine with delegate rest structure

--- camunda-engine/src/main/java/com/example/delegate/Application.java
+++ camunda-engine/src/main/java/com/example/delegate/Application.java
@@ -1,8 +1,10 @@
-package com.example.workflow;
+package com.example.delegate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
---
+++ camunda-engine/src/main/java/com/example/delegate/domain/Book/Book.java
@@ -0,0 +1,12 @@
+package com.example.delegate.domain.Book;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+    private int id;
+    private String name;
+    private double price;
+}
